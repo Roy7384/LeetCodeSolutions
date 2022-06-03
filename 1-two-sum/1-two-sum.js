3e20585324f7d9300bb6306b13a81361eb3103ff
Roy Liu <roylu7384@gmail.com>
2022-06-03 13:23:03 -0400

    Time: 97 ms (64.81%), Space: 42.5 MB (60.56%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,13 +5,16 @@
  */
 var twoSum = function(nums, target) {
     
-    for (let i = 0, n = nums.length; i < n; i ++) {
-        const num = nums[i];
-        const restArr = nums.slice(i + 1);
-        for (let j = 0, m = restArr.length; j < m; j ++) {
-            if ((num + restArr[j]) === target) {
-                return [i, i + j + 1]
-            }
-        }
+const table = {};
+  
+  for (let i = 0, n = nums.length; i < n; i++) {
+    const num = nums[i];
+    const diff = target - num;
+    
+    if (table[diff] >= 0) {
+      return [table[diff], i];
+    } else {
+      table[num] = i;
     }
+  }
 };
