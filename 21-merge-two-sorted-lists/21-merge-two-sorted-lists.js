c963fd2a7d33af839b5872620ae8d643ba099260
Roy Liu <roylu7384@gmail.com>
2022-06-03 16:40:10 -0400

    Time: 87 ms (55.97%), Space: 44.4 MB (40.25%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,7 +11,7 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-  const dummy = new ListNode(-Infinity);
+  const dummy = {next: null};
   let prevNode = dummy;
 
   while (list1 && list2) {
