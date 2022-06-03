7bc5ef24afc9f0c6b34afafa838a94c4dbfe0c43
Roy Liu <roylu7384@gmail.com>
2022-06-03 14:29:14 -0400

    Time: 77 ms (60.11%), Space: 42.1 MB (76.70%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+      const bracketParis = {
+    ")": "(",
+    "}": "{",
+    "]": "["
+  };
+
+  const openBrackets = ["(", "{", "["];
+  const keepTrackOpen = [];
+  
+  for (let i = 0, n = s.length; i < n; i++) {
+    const char = s[i];
+    if (openBrackets.includes(char)) {
+      keepTrackOpen.push(char);
+    } else if (bracketParis[char] === keepTrackOpen.pop()) {
+      continue;
+    } else {
+      return false;
+    }
+  }
+  
+  if (keepTrackOpen.length) {
+    return false;
+  }
+
+  return true;
+};
