38fbafdf96b1f4055ee97641ee0b4672708618a8
Roy Liu <roylu7384@gmail.com>
2022-06-04 15:01:59 -0400

    Time: 166 ms (5.51%), Space: 50.7 MB (22.38%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,27 +3,15 @@
  * @return {number}
  */
 const maxSubArray = function(nums) {
-  let l = 0;
-  let r = 0;
   const arrLen = nums.length;
-  let sum;
+  let sum = 0;
   let maxSum = nums[0];
   
-  while (r < arrLen) {
-      
-    if (l === r) {
-      sum = nums[l];
-    } else {
-      sum = sum + nums[r];
-    }
-    
-    r ++;
-      
-    if (sum < 0) l = r;
-    maxSum = maxSum > sum ? maxSum : sum;
-    
+  for (const num of nums) {
+      if (sum < 0) sum = 0;
+      sum += num;
+      maxSum = maxSum >= sum ? maxSum : sum;
   }
-  
     
   return maxSum;
 };
