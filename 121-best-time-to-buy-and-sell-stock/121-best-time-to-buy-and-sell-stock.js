8e779619b7d768a5b29ff528185d61e8c26753e6
Roy Liu <roylu7384@gmail.com>
2022-06-03 22:18:50 -0400

    Time: 92 ms (80.57%), Space: 51.9 MB (31.71%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,7 +2,7 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
+const maxProfit = function(prices) {
   let profit = 0;
   
   let buyDay = 0;
@@ -11,8 +11,11 @@ var maxProfit = function(prices) {
   const totalDays = prices.length;
   
   while (sellDay <= prices.length) {
-    if (prices[sellDay] > prices[buyDay]) {
-      profit = Math.max(profit, prices[sellDay] - prices[buyDay])
+    const buyPrice = prices[buyDay];
+    const sellPrice = prices[sellDay];
+    
+    if (sellPrice > buyPrice) {
+      profit = Math.max(profit, sellPrice - buyPrice)
     } else {
       buyDay = sellDay;
     }
