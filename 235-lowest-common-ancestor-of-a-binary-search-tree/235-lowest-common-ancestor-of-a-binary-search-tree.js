aff7f83201d6455186baddcdf0a47d1af935f513
Roy Liu <roylu7384@gmail.com>
2022-06-04 16:24:05 -0400

    Time: 115 ms (47.66%), Space: 51.9 MB (82.08%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -14,15 +14,20 @@
  */
 
 var lowestCommonAncestor = function(root, p, q) {
-    const valP = p.val;
-    const valQ = q.val;
-    const valRoot = root.val;
 
-    if(valP > valRoot && valQ > valRoot) {
-       return lowestCommonAncestor(root.right, p, q);
-    } else if (valP < root.val && valQ < root.val) {
-       return lowestCommonAncestor(root.left, p, q);
-    } else {
-       return root
-    }
+    let currentNode = root;
+    
+    while (currentNode) {
+        const valP = p.val;
+        const valQ = q.val;
+        const valCur = currentNode.val;
+        
+        if (valP > valCur && valQ > valCur) {
+            currentNode = currentNode.right;
+        } else if (valP < valCur && valQ < valCur) {
+            currentNode = currentNode.left;
+        } else {
+            return currentNode;
+        }
+    } 
 };
