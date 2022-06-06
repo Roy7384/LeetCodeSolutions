159c76b3dca9b4bc4c1526ce25fe26c514a656a5
Roy Liu <roylu7384@gmail.com>
2022-06-06 15:57:16 -0400

    Time: 105 ms (42.07%), Space: 44.1 MB (71.63%) - LeetHub

--- 57-insert-interval/57-insert-interval.js
+++ 57-insert-interval/57-insert-interval.js
@@ -1,52 +1,26 @@
-const bSearch = function(arr, target) {
-  if (!arr.length || target < arr[0][0]) return -1;
-  
-  let left = 0;
-  let right = arr.length - 1;
-  let mid;
-  
-  while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-    const midNum = arr[mid][0];
-      
-    if (target > midNum) {
-      left = mid + 1;
-    } else if (target < midNum) {
-      right = mid - 1;
-    } else {
-      return mid;
-    }
-  }
-  return left - 1;
-};
-
-const insert = function(intervals, newInterval) {
-  
-  if (!intervals.length) return [newInterval];
-  // if newInterval is completely out of range of intervals
-  if (newInterval[1] < intervals[0][0]) return [newInterval].concat(intervals);
-  if (newInterval[0] > intervals[intervals.length - 1][1]) {
-    intervals.push(newInterval);
-    return intervals;
-  }
-  
-  const [s, e] = newInterval;
-  
-  const sPosition = bSearch(intervals, s);
-  const ePosition = bSearch(intervals, e);
-  
-  const toBeInsertEnd = Math.max(intervals[ePosition][1], newInterval[1]);
-  
-  if (sPosition === -1) {
-    const headInt = [newInterval[0], toBeInsertEnd];
-    return [headInt].concat(intervals.slice(ePosition + 1));
-  }
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    const result = [];
     
-  if (newInterval[0] > intervals[sPosition][1]) {
-    const toBeInsert = [newInterval[0], toBeInsertEnd];
-    return intervals.slice(0, sPosition + 1).concat([toBeInsert]).concat(intervals.slice(ePosition + 1));
-  } else {
-    const toBeInsert = [intervals[sPosition][0], toBeInsertEnd];
-    return intervals.slice(0, sPosition).concat([toBeInsert]).concat(intervals.slice(ePosition + 1));
-  }
+    for (let i = 0, n = intervals.length; i < n; i++) {
+        if (newInterval[1] < intervals[i][0]) {
+            result.push(newInterval);
+            return result.concat(intervals.slice(i));
+        }
+        
+        if (newInterval[0] > intervals[i][1]) {
+            result.push(intervals[i]);
+            continue;
+        }
+        
+        const newStart = Math.min(newInterval[0], intervals[i][0]);
+        const newEnd = Math.max(newInterval[1], intervals[i][1]);
+        newInterval = [newStart, newEnd];
+    }
+    result.push(newInterval); 
+    return result;
 };
