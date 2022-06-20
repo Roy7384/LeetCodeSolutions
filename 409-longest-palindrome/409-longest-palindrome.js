582706ee823074d361e55640e11854ed17dba941
Roy Liu <roylu7384@gmail.com>
2022-06-20 15:56:27 -0400

    Time: 130 ms (10.98%), Space: 43.8 MB (66.31%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.js
+++ 409-longest-palindrome/409-longest-palindrome.js
@@ -3,33 +3,22 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    const charCount = {}
-    for (let i = 0, n = s.length; i < n; i++) {
-        const char = s[i];
-        
-        if (charCount[char]) {
-            charCount[char] ++
-        } else {
-            charCount[char] = 1
-        }
-    }
+    const charLookup = {};
     
     let result = 0;
     
-    for (const char in charCount) {
-        if (charCount[char] > 1) {
-            const usableCount = charCount[char] - charCount[char] % 2
-            charCount[char] -= usableCount;
-            result += usableCount;
+    for (let i = 0, n = s.length; i < n; i++) {
+        const char = s[i];
+        
+        if (!charLookup[char]) {
+            charLookup[char] = 1
+        } else {
+            delete charLookup[char]
+            result += 2;
         }
     }
     
-    for (const char in charCount) {
-        if (charCount[char] === 1) {
-           result ++;
-            break;
-        }
-    } 
+    if (Object.keys(charLookup).length) result ++;
     
     return result;
 };
