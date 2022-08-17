5bc8bc940b80780c4dc6e1ca780aeb8f393ab87a
Roy Liu <roylu7384@gmail.com>
2022-08-17 09:08:34 -0400

    Time: 183 ms (5.13%), Space: 210.9 MB (85.98%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.rb
+++ 20-valid-parentheses/20-valid-parentheses.rb
@@ -7,15 +7,12 @@ def is_valid(s)
   s.each_char do |c|
     if open_bracket.include?(c)
       keep_track_open.push(c)
-    elsif bracket_paris.keys.include?(c)
-        if bracket_paris[c] == keep_track_open[-1]
-            keep_track_open.pop
-        else
-            return false
-        end
+    elsif bracket_paris[c] == keep_track_open.pop
+      next
+    else
+      return false
     end
   end
 
   keep_track_open.empty?
-
 end
