ff0eacb18f7885663605d63ab28d66a6a7fcaaea
Roy Liu <roylu7384@gmail.com>
2022-08-17 08:23:57 -0400

    Time: 163 ms (30.72%), Space: 212.2 MB (33.22%) - LeetHub

---
+++ 1-two-sum/1-two-sum.rb
@@ -0,0 +1,12 @@
+ def two_sum(nums, target)
+      diff_lookup = {}
+   
+    nums.each_with_index do |num, i|
+        diff = target - num
+       if diff_lookup[diff].nil?
+         diff_lookup[num] = i
+       else
+         return [diff_lookup[diff], i]
+       end
+     end
+   end
