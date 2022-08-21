6620ca42175a768e1b6ac1e96c7ff0b64dd7f3c0
Roy Liu <roylu7384@gmail.com>
2022-08-21 17:02:31 -0400

    Time: 96 ms (93.98%), Space: 212 MB (91.16%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.rb
+++ 141-linked-list-cycle/141-linked-list-cycle.rb
@@ -12,12 +12,14 @@
 def hasCycle(head)
     return false unless head 
     slow_pointer = head
-    fast_pointer = head
+    return false unless head.next
+    fast_pointer = head.next
     
     while fast_pointer && fast_pointer.next
+        return true if slow_pointer == fast_pointer
+        
         slow_pointer = slow_pointer.next
         fast_pointer = fast_pointer.next.next 
-        return true if slow_pointer == fast_pointer
     end
     
     false
