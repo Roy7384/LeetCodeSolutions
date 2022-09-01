c67d0258d89d2164cbfa00e9388a45b81abfa720
Roy Liu <roylu7384@gmail.com>
2022-08-31 22:01:09 -0400

    Time: 159 ms (7.89%), Space: 211.2 MB (13.80%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.rb
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.rb
@@ -10,11 +10,11 @@
 # @return {ListNode}
 def middle_node(head)
     slow_p = head
-    fast_p = head.next
+    fast_p = head
     
-    while fast_p 
+    while fast_p && fast_p.next 
         slow_p = slow_p.next
-        fast_p = fast_p.next.nil? ? nil : fast_p.next.next
+        fast_p = fast_p.next.next
     end
     
     slow_p
