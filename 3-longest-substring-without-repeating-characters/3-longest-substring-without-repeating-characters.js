f992c650328d8b0f5dcf6668ac2b593d0dc66575
Roy Liu <roylu7384@gmail.com>
2022-09-04 14:23:49 -0400

    Time: 453 ms (20.08%), Space: 48.9 MB (34.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,24 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let result = [] 
+    let result = new Set(); 
     
     for (let i = 0, n = s.length; i < n; i++) {
-        const tempResult = [s[i]];
+        const tempResult = new Set();
+        tempResult.add(s[i])
         
         for (let j = i + 1; j < n; j++) {
-            if (tempResult.includes(s[j])) {
+            if (tempResult.has(s[j])) {
                 break
             } else {
-                tempResult.push(s[j])
+                tempResult.add(s[j])
             }
         }
         
-        if (tempResult.length > result.length) {
+        if (tempResult.size > result.size) {
             result = tempResult
         }
         
     }
     
-    return result.length
+    return result.size
 };
