7068cbc95b1c974abfbee42a0ff31a1cd28f7818
Roy Liu <roylu7384@gmail.com>
2022-09-13 07:02:43 -0400

    Time: 122 ms (10.98%), Space: 45.2 MB (5.56%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,50 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    if (!node) return null;
+    const visited = new Set();
+    const clonedNodes = [];
+    
+    const queue = [node];
+    
+    while(queue.length) {
+        const n = queue.shift();
+        if(visited.has(n.val)) continue;
+        
+        visited.add(n.val);
+        
+        let clonedNode;
+        
+        if (!clonedNodes[n.val]) {
+            clonedNode = new Node(n.val);
+            clonedNodes[n.val] = clonedNode;
+        } else {
+            clonedNode = clonedNodes[n.val]
+        }
+        
+        for (let nei of n.neighbors) {
+            queue.push(nei);
+            
+            if (visited.has(nei.val) || clonedNodes[nei.val]) {
+                clonedNode.neighbors.push(clonedNodes[nei.val])
+            } else {
+                const newNei = new Node(nei.val);
+                clonedNodes[nei.val] = newNei;
+                clonedNode.neighbors.push(clonedNodes[nei.val])
+            }
+        }
+    }
+    
+    return clonedNodes[1]
+};
+
