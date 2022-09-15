ace7c236109c6c71fdf317eba7c405caad546e50
Roy Liu <roylu7384@gmail.com>
2022-09-14 20:48:44 -0400

    Time: 120 ms (56.66%), Space: 46.2 MB (87.47%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -9,23 +9,12 @@ var canFinish = function(numCourses, prerequisites) {
     const visited = new Set();
     
     for (let key in adjList) {
-        if (!dfs(visited, adjList, key)) return false
+        if (!dfs(visited, adjList, Number(key))) return false
+        visited.delete(Number(key));
     }
     
     return true;
 };
-
-
-const buildAdjList = function(arr) {
-    const adj = {};
-    
-    for (let relation of arr) {
-        if (!(relation[0] in adj)) adj[relation[0]] = [];
-        adj[relation[0]].push(relation[1]);
-    }
-    
-    return adj;
-}
 
 const dfs = function(visited, adjList, currentCourse) {
     if (visited.has(currentCourse)) return false;
@@ -38,8 +27,18 @@ const dfs = function(visited, adjList, currentCourse) {
         if (!dfs(visited, adjList, nei)) return false
         visited.delete(nei);
     }
-    adjList[currentCourse] = undefined;
+    adjList[currentCourse] = null;
     return true;
 }
 
+const buildAdjList = function(arr) {
+    const adj = {};
+    
+    for (let relation of arr) {
+        if (!(relation[0] in adj)) adj[relation[0]] = [];
+        adj[relation[0]].push(relation[1]);
+    }
+    
+    return adj;
+}
 
