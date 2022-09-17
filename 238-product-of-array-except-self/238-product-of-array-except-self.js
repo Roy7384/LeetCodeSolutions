03b387789ec6c45b370adecbc4c5d1727828429a
Roy Liu <roylu7384@gmail.com>
2022-09-17 10:35:28 -0400

    Time: 120 ms (86.49%), Space: 55.7 MB (31.12%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let productFromFront = 1;
+    let accumulator = 1;
     const result = [] 
     for (let i = 0, n = nums.length; i < n; i++) {
-        result[i] = productFromFront;
-        productFromFront *= nums[i];
+        result[i] = accumulator;
+        accumulator *= nums[i];
     }
     
-    let productFromBack = 1;
+    accumulator = 1;
     for (let j = nums.length - 1; j >= 0; j--) {
-        result[j] *= productFromBack;
-        productFromBack *= nums[j];
+        result[j] *= accumulator;
+        accumulator *= nums[j];
     }
     
     return result;
