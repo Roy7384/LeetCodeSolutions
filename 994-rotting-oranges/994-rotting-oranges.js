7be45bad9529a682f0701eff4a994005cc7c8be2
Roy Liu <roylu7384@gmail.com>
2022-09-18 14:59:45 -0400

    Time: 132 ms (37.17%), Space: 45 MB (50.62%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -4,36 +4,23 @@
  */
 var orangesRotting = function(grid) {
     let fresh = 0;
-    const visited = new Set(); 
     const queue = [];
     
     for (let i = 0, n = grid.length; i < n; i++) {
         for (let j = 0, m = grid[0].length; j < m; j++) {
             const val = grid[i][j];
-            
-            if (val === 1) {
-                fresh++
-            } else if (val === 2) {
-                queue.push([i, j])
-                visited.add(`${i},${j}`)
-            } else {
-                visited.add(`${i},${j}`)
-            }
+            if (val === 1) fresh++;
+            if (val === 2) queue.push([i, j])
         }
     }
     
     const direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     let time = 0;
     
-    while(queue.length > 0) {
-        const childSize = queue.length;
-        for (let i = 0; i < childSize; i++) {
+    while(queue.length > 0 && fresh > 0) {
+        const nodeSize = queue.length;
+        for (let i = 0; i < nodeSize; i++) {
             const [row, col] = queue.shift();
-            
-            if (grid[row][col] === 1) {
-                grid[row][col] = 2;
-                fresh--
-            }
             
             direction.forEach((d) => {
                 const newRow = row + d[0];
@@ -42,16 +29,18 @@ var orangesRotting = function(grid) {
                 const rowInBound = newRow >= 0 && newRow < grid.length;
                 const colInBound = newCol >= 0 && newCol < grid[0].length;
                 
-                if (rowInBound && colInBound && !visited.has(`${newRow},${newCol}`)) {
-                    queue.push([newRow, newCol]);
-                    visited.add(`${newRow},${newCol}`);
-                }
+                if (rowInBound && colInBound && grid[newRow][newCol] === 1) {
+                    grid[newRow][newCol] = 2;
+                    fresh--;
+                    
+                    queue.push([newRow, newCol]);  
+                } 
             })
         }
         time ++ 
     }
     if (fresh > 0) return -1;
-    return time ? time - 1 : 0;
+    return time;
 };
 
 
