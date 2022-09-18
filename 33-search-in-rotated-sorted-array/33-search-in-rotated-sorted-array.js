8cfc11eb3ba55d42637f79c56db5d09f92fc4f49
Roy Liu <roylu7384@gmail.com>
2022-09-18 17:01:42 -0400

    Time: 107 ms (24.09%), Space: 41.9 MB (86.04%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,61 +4,29 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    if (nums[0] <= nums[nums.length -1]) {
-        return binarySearch(nums, target)
-    } else {
-        const k = findK(nums);
-        return binarySearch(nums, target, k);
-    }
-};
-
-
-
-const binarySearch = function(arr, target, k) {
-    let l, r;
-    if (k !== undefined) {
-       if (arr[0] <= target) {
-           l = 0;
-           r = k;
-       } else {
-           l = k + 1;
-           r = arr.length - 1
-       } 
-    } else {
-        l = 0;
-        r = arr.length - 1;
-    }
+    let l = 0;
+    let r = nums.length - 1;
     
     while (l <= r) {
         const mid = Math.floor((l + r) / 2);
-        const midVal = arr[mid];
+        const midVal = nums[mid];
         
-        if (midVal > target) {
-            r = mid - 1;
-        } else if (midVal < target) {
-            l = mid + 1;
-        } else {
-            return mid;
-        }
-    } 
-    return -1;
-}
-
-const findK = function(arr) {
-    let l = 0;
-    let r = arr.length - 1;
-    let mid;
-    while (l <= r) {
-        mid = Math.floor((l + r) / 2);
-        const midVal = arr[mid]
+        if (midVal === target) return mid;
         
-        if (midVal < arr[mid - 1]) {
-            return mid - 1;
-        } else if (midVal > arr[0]) {
-            l = mid + 1;
+        if (target > midVal) {
+            if (midVal < nums[0] && target > nums[nums.length - 1]) {
+                r = mid - 1; 
+            } else {
+                l = mid + 1;
+            }
         } else {
-            r = mid - 1;
+            if (midVal > nums[nums.length - 1] && target < nums[0]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
     }
-    return mid;
+    
+    return -1;
 }
