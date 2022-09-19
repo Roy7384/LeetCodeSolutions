1665ce4c5f71f9f8945013963ee48ef7d7628c70
Roy Liu <roylu7384@gmail.com>
2022-09-19 18:15:56 -0400

    Time: 685 ms (43.30%), Space: 106.4 MB (6.88%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.js
@@ -0,0 +1,35 @@
+
+var TimeMap = function() {
+    this.map = {};
+};
+
+/** 
+ * @param {string} key 
+ * @param {string} value 
+ * @param {number} timestamp
+ * @return {void}
+ */
+TimeMap.prototype.set = function(key, value, timestamp) {
+    if (!(key in this.map)) this.map[key] = {};
+    this.map[key][timestamp] = value;
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} timestamp
+ * @return {string}
+ */
+TimeMap.prototype.get = function(key, timestamp) {
+    if (!this.map[key]) return "" 
+    for (let time = timestamp; time >= 0; time--) {
+        if (time in this.map[key]) return this.map[key][time]
+    }
+    return ""
+};
+
+/** 
+ * Your TimeMap object will be instantiated and called as such:
+ * var obj = new TimeMap()
+ * obj.set(key,value,timestamp)
+ * var param_2 = obj.get(key,timestamp)
+ */
