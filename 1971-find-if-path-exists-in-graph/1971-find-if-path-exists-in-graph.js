0399617bbc4e277233a93ef711824ffde770fa70
Roy Liu <roylu7384@gmail.com>
2022-09-20 21:55:00 -0400

    Time: 579 ms (84.45%), Space: 111.7 MB (83.73%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -9,24 +9,25 @@ var validPath = function(n, edges, source, destination) {
     const vertices = new Array(n).fill(null);
     const groups = vertices.map((_, index) => index); 
     
-    for (const edge of edges) {
-        const [v1, v2] = edge; 
-        union(v1, v2, groups);
+    const union = (x, y) => {
+        let rootX = find(x);
+        let rootY = find(y);
+
+        if (rootX !== rootY) groups[rootX] = rootY;
+    }
+
+    const find = (x) => {
+        if (x === groups[x]) return x
+
+        return find(groups[x]);
     }
     
-    return find(source, groups) === find(destination, groups);
+    for (const edge of edges) {
+        const [v1, v2] = edge; 
+        union(v1, v2);
+    }
+    
+    return find(source) === find(destination);
 };
 
 
-const union = (x, y, group) => {
-    let rootX = find(x, group);
-    let rootY = find(y, group);
-    
-    if (rootX !== rootY) group[rootX] = rootY;
-}
-
-const find = (x, group) => {
-    if (x === group[x]) return x
-    
-    return find(group[x], group);
-}
