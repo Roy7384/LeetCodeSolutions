7bfa24eb1e3defe1fe61a1ec314a0c92371eda3a
Roy Liu <roylu7384@gmail.com>
2022-09-20 21:52:10 -0400

    Time: 590 ms (83.26%), Space: 97.7 MB (88.88%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -6,47 +6,27 @@
  * @return {boolean}
  */
 var validPath = function(n, edges, source, destination) {
-
-    const adjList = {};
+    const vertices = new Array(n).fill(null);
+    const groups = vertices.map((_, index) => index); 
     
     for (const edge of edges) {
-        const [v1, v2] = edge;
-        
-        if (!(v1 in adjList)) adjList[v1] = [];
-        if (!(v2 in adjList)) adjList[v2] = [];
-        adjList[v1].push(v2);
-        adjList[v2].push(v1);
+        const [v1, v2] = edge; 
+        union(v1, v2, groups);
     }
     
-    const stack = [source];
-    const visited = new Set();
+    return find(source, groups) === find(destination, groups);
+};
+
+
+const union = (x, y, group) => {
+    let rootX = find(x, group);
+    let rootY = find(y, group);
     
-    while(stack.length > 0) {
-        const currentNode = stack.pop();
-        
-        if (currentNode === destination) return true;
-        
-        if (visited.has(currentNode)) continue;
-        visited.add(currentNode);
-        
-        for (let neigh of adjList[currentNode]) {
-            stack.push(neigh);
-        }
-    }
-    
-    return false;
+    if (rootX !== rootY) group[rootX] = rootY;
 }
 
-/*
-const dfs = function(node, adjList, visited, destination) {
-    if (visited.has(node)) return;
-    if (node === destination) return true;
-    visited.add(node);
+const find = (x, group) => {
+    if (x === group[x]) return x
     
-    for (const neigh of adjList[node]) {
-       if (dfs(neigh, adjList, visited, destination)) return true;
-    }
-    
-    return false;
+    return find(group[x], group);
 }
-*/
