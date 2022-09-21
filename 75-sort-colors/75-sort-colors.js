00ac519416fc8b738aa9c308e359c58480212938
Roy Liu <roylu7384@gmail.com>
2022-09-20 22:10:03 -0400

    Time: 98 ms (40.34%), Space: 41.8 MB (81.24%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    
+    const count = {
+        0: 0,
+        1: 0,
+        2: 0
+    }
+    
+    for (const num of nums) {
+        count[num]++
+    }
+    
+    for (let i = 0, n = nums.length; i < n; i++) {
+        if (i <= count[0] - 1) {
+            nums[i] = 0;
+            continue;
+        }
+        if (i < count[0] + count[1]) {
+            nums[i] = 1;
+            continue;
+        }
+        nums[i] = 2;
+    }
+};
