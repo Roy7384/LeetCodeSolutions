539131d6c9a82cbc79da88ac0f8b51e62b274e92
Roy Liu <roylu7384@gmail.com>
2022-09-21 23:14:05 -0400

    Time: 74 ms (75.98%), Space: 42.3 MB (20.47%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+var wordBreak = function(s, wordDict) {
+    let dp = new Array(s.length + 1).fill(undefined);
+    dp = dp.map(_ => []);
+    
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (const word of wordDict) {
+            const tempWord = s.slice(i, i + word.length);
+            
+            if (s.length - i === word.length) {
+                
+                if (tempWord === word) {
+                    dp[i].push([word]);         
+                }
+            }
+            
+            if (s.length - i > word.length && tempWord === word) { 
+                for (const combination of dp[i + word.length]) {
+                    dp[i].push([word, ...combination]);
+                } 
+            }
+        }
+    }
+    
+    const finalResult = dp[0].map(combination => combination.join(" "))
+
+    return finalResult;
+};
