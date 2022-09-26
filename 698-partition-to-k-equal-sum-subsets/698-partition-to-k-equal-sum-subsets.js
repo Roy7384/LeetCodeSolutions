a7061180998e9b97a8e6a32356c13263db93f766
Roy Liu <roylu7384@gmail.com>
2022-09-26 17:12:51 -0400

    Time: 8011 ms (9.24%), Space: 42.6 MB (65.22%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var canPartitionKSubsets = function(nums, k) {
+    const target = nums.reduce((sum, num) => sum += num) / k;
+    if (target !== Math.floor(target)) return false;
+    
+    const used = new Array(nums.length).fill(false);
+        
+    const backTrack = function (i, k, subsetSum) {
+        if (k === 0) return true;
+        
+        if (subsetSum === target) return backTrack(0, k - 1, 0);
+        
+        for (let j = i, n = nums.length; j < n; j++) {
+            if (used[j] || subsetSum + nums[j] > target) continue;
+            used[j] = true;
+            if (backTrack(j + 1, k, subsetSum + nums[j])) return true;
+            used[j] = false;
+        }
+        return false;
+    }
+    
+    return backTrack(0, k, 0);
+};
