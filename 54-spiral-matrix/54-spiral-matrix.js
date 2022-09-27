5f26f8b180a415e202b35f9e5b0061363a5a3422
Roy Liu <roylu7384@gmail.com>
2022-09-26 21:56:24 -0400

    Time: 101 ms (25.68%), Space: 41.7 MB (72.23%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.js
+++ 54-spiral-matrix/54-spiral-matrix.js
@@ -19,8 +19,8 @@ var spiralOrder = function(matrix) {
     visited.add(row + ',' + col)
     
     while (result.length < totalElement) {
-        let newRow = directions[turnCount % 4][0] + row;
-        let newCol = directions[turnCount % 4][1] + col;
+        let newRow = directions[turnCount][0] + row;
+        let newCol = directions[turnCount][1] + col;
         
         const rowInB = newRow >= 0 && newRow < rowBound; 
         const colInB = newCol >= 0 && newCol < colBound; 
@@ -28,6 +28,7 @@ var spiralOrder = function(matrix) {
         
         if(!rowInB || !colInB || visited.has(mark)) {
             turnCount++;
+            turnCount = turnCount % 4;
         } else {
             result.push(matrix[newRow][newCol]);
             visited.add(mark);
