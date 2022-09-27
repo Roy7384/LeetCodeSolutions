5da87d0cbf05ba8b1661e6f4bae7b9faafde2fe8
Roy Liu <roylu7384@gmail.com>
2022-09-27 14:52:48 -0400

    Time: 90 ms (54.86%), Space: 41.9 MB (82.53%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,16 +4,13 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const table = new Array(m + 1).fill().map(_ => new Array(n + 1).fill(0));
-    table[1][1] = 1; 
+    const table = new Array(m).fill().map(_ => new Array(n).fill(1));
     
-    for (let i = 1; i < m + 1; i++) {
-        for (let j = 1; j < n + 1; j++) {
-            if (i === 1 && j === 1) continue;
-            
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
             table[i][j] = table[i - 1][j] + table[i][j - 1];
         }
     } 
     
-    return table[m][n];
+    return table[m - 1][n - 1];
 };
