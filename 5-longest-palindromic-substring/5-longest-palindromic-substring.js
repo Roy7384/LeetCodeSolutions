75f015be31d3d7a8f066177b4dfc7a5fc70e5148
Roy Liu <roylu7384@gmail.com>
2022-09-27 12:23:48 -0400

    Time: 130 ms (77.21%), Space: 43.3 MB (89.22%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -3,24 +3,30 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    let result = "";
+    let start = 0;
+    let end = 0;
+    
     const scanOutWard = function(l, r, n) {
         while (l >= 0 && r < n && s[l] === s[r]) {
-            if (r - l + 1 > result.length) result = s.slice(l, r + 1);
             l --;
             r ++;
         }
+        return r - l - 1;
     }
     
     for (let i = 0, n = s.length; i < n; i++) {
-        let l = i;
-        let r = i;
-        scanOutWard(l, r, n);
+        // odd length
+        const len1 = scanOutWard(i, i, n);
+        // even length
+        const len2 = scanOutWard(i, i + 1, n);
         
-        l = i;
-        r = i + 1;
-        scanOutWard(l, r, n);
+        const len = Math.max(len1, len2);
+        
+        if (len > end - start + 1) {
+            start = i - Math.floor(((len - 1) / 2));
+            end = i + Math.floor((len / 2));
+        }
     }
     
-    return result;
+    return s.slice(start, end + 1);
 };
