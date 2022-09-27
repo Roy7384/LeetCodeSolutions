467c082d3b33b6d2f8ae539fe722affa6c672e52
Roy Liu <roylu7384@gmail.com>
2022-09-26 22:57:57 -0400

    Time: 120 ms (15.94%), Space: 43.2 MB (68.98%) - LeetHub

---
+++ 78-subsets/78-subsets.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    
+    const result = [[]];
+    
+    for (let i = nums.length; i > 0; i--) {
+        backTrack(0, [], result, nums.slice(0, i));     
+    }
+    
+    return result
+    
+};
+
+const backTrack = function(i, tempResult, result, nums) {
+    if (i === nums.length) {
+        result.push([...tempResult]);
+        return;
+    }
+
+    for (let j = i, n = nums.length; j < n; j++) {
+        tempResult.push(nums[j]);
+        backTrack(j + 1, tempResult, result, nums);
+        tempResult.pop();
+    }
+}
