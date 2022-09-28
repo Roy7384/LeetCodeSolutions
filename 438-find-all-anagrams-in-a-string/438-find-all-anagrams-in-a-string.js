6f1fd771e4a96893fcbecc163de06645ffd0471e
Roy Liu <roylu7384@gmail.com>
2022-09-28 15:30:18 -0400

    Time: 360 ms (25.20%), Space: 48.2 MB (53.92%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -4,37 +4,42 @@
  * @return {number[]}
  */
 var findAnagrams = function(s, p) {
+    if (p.length > s.length) return [];
+    
     const charCount = {};
-    for (let char of p) {
-        if (!(char in charCount)) charCount[char] = 0;
-        charCount[char]++;
+    const subCount = {}; 
+    
+    for (let i = 0, n = p.length; i < n; i++) {
+        if (!(p[i] in charCount)) charCount[p[i]] = 0;
+        charCount[p[i]]++;
+        
+        if (!(s[i] in subCount)) subCount[s[i]] = 0;
+        subCount[s[i]]++;
     }
-    const compare = function(i) {
-        const subCount = {};
-
-        for (let j = i; j < i + p.length; j++) {
-            const char = s[j];
-            if (!(char in charCount)) return j;
-            if (!(char in subCount)) subCount[char] = 0;
-            subCount[char] ++;
-            if (subCount[char] > charCount[char]) return false
+    
+    const compare = function() {
+        for (let char in subCount) {
+            if (!(char in charCount)) return false;
+            if (subCount[char] !== charCount[char]) return false;
         }
-
         return true;
     }
     
-    const result = []; 
-    for (let i = 0, n = s.length; i < n; i++) {
-        if (s.length - i < p.length) return result;
-       
-        const compareRes = compare(i);
-        if (typeof compareRes === "number") {
-            i = compareRes;
-            continue;
-        }
-        if (compareRes === true) result.push(i);
-    }
+    const result = [];
+    let l = 0;
+    let r = p.length - 1;
     
+    while (r < s.length) {
+        if (compare()) result.push(l);
+        
+        subCount[s[l]]--;
+        if (subCount[s[l]] === 0) delete subCount[s[l]]
+        l++;
+        r++;
+        if (!(s[r] in subCount)) subCount[s[r]] = 0;
+        subCount[s[r]]++;
+    }
+     
     return result;
 };
 
