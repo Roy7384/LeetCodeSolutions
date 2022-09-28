2933248f1af213ed8b75f1f25c14fe6c0a5afded
Roy Liu <roylu7384@gmail.com>
2022-09-28 13:41:25 -0400

    Time: 1114 ms (38.77%), Space: 48.4 MB (39.00%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,31 +4,33 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const used = new Set();
+    const path = new Set();
+    const rowB = board.length;
+    const colB = board[0].length;
     
     const search = function(row, col, i) { 
         if (i === word.length) return true;
         
-        const rowInB = row >= 0 && row < board.length; 
-        const colInB = col >= 0 && col < board[0].length; 
+        const rowInB = row >= 0 && row < rowB; 
+        const colInB = col >= 0 && col < colB; 
         if (!rowInB || !colInB) return false;
         
         if (board[row][col] !== word[i]) return false;
         
         const mark = row + ',' + col;
-        if (used.has(mark)) return false;
-        used.add(mark);
+        if (path.has(mark)) return false;
+        path.add(mark);
     
         const res = (
             search(row - 1, col, i + 1) || search(row + 1, col, i + 1) || search(row, col + 1, i + 1) || search(row, col - 1, i + 1)
         );
-        used.delete(mark); 
+        path.delete(mark); 
         
         return res;
     }
     
-    for (let i = 0, n = board.length; i < n; i++) {
-        for (let j = 0, m = board[0].length; j < m; j++) {
+    for (let i = 0, n = rowB; i < n; i++) {
+        for (let j = 0, m = colB; j < m; j++) {
             if (board[i][j] === word[0]) {
                 const searchRes = search(i, j, 0);
                 if (searchRes) return true;
