2f0b470347865a733bd7ae6238a702c36a242f1f
Roy Liu <roylu7384@gmail.com>
2022-10-04 14:11:52 -0400

    Time: 129 ms (91.14%), Space: 54.4 MB (20.97%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+const NULL_NODE = 'N';
+
+var serialize = function(root) {
+    if(!root) return '';
+    let stack = [];
+    
+    const dfs = (node) => {
+        if(!node) {
+            stack.push(NULL_NODE);
+            return;
+        }
+        stack.push(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+    dfs(root);
+    return stack.join(',');
+};
+
+var deserialize = function(data) {
+    if(!data.length) return null;
+    let fData = data.split(',');
+    let ptr = 0;
+    
+    const dfs = () => {
+        if(fData[ptr] === NULL_NODE) {
+            ptr++;
+            return null;
+        }
+        let node = new TreeNode(parseInt(fData[ptr]));
+        ptr++;
+        node.left = dfs();
+        node.right = dfs();
+        return node;
+    }
+    
+    return dfs();
+}
+
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
